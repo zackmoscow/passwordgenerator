5d2c9e149cea1ce006f52a7887f425e14be3d0c7
Zack Moscow <zmoscow@gmail.com>
2019-11-05 16:10:44 -0600

    finished generate password function in JS; minor style edits in HTML; README write up

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # passwordgenerator
+
+## purpose
+create a password generator with the following parameters:
+1. length between 8 and 128 characters
+2. selectable character types (special, numeric, lowercase, uppercase)
+3. user input validation
+4. display password on screen
+5. copy to clipboard functionality
+
+## html execution
+build base html via bootstrap components 
+1. header
+2. input body with range slider and checkbox forms
+3. text field for password display
+4. buttons for generate and copy to clipboard)
+
+## js execution
+1. define character arrays
+2. generate password function
+    a. "if/else if" tree to build password array based on 16 possible checkbox combinations, including displaying an alert if user input isn't valid
+    b. for loop to generate password based on slider length value
+    c. print generated password back to html textarea
+3. copy to clipboard function
+
+## screenshot
+
+
+
+## deployed URL
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var specialCharacters = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -7,12 +7,57 @@ var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"
 var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 function generateFunction() {
-    document.getElementById("test").innerHTML = "PASSWORD PLACEHOLDER";
-}
+
+    var storedValue = "";
+    
+    if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === false){
+        alert("You must select at least one character type.");
+    } else if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === true){
+        var passwordCharacters = lowercaseCharacters;
+    } else if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = uppercaseCharacters;
+    } else if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = numericCharacters;
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = specialCharacters;
+    } else if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === true){
+        var passwordCharacters = lowercaseCharacters.concat(uppercaseCharacters);
+    } else if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === true){
+        var passwordCharacters = lowercaseCharacters.concat(numericCharacters);
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === true){
+        var passwordCharacters = lowercaseCharacters.concat(specialCharacters);
+    } else if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = numericCharacters.concat(uppercaseCharacters);
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = specialCharacters.concat(uppercaseCharacters);
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = numericCharacters.concat(specialCharacters);
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = numericCharacters.concat(specialCharacters, uppercaseCharacters);
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === false && includeLowercaseCharacters.checked === true){
+        var passwordCharacters = numericCharacters.concat(specialCharacters, lowercaseCharacters);
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === false && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === true){
+        var passwordCharacters = lowercaseCharacters.concat(specialCharacters, uppercaseCharacters);
+    } else if (includeSpecialCharacters.checked === false && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === true){
+        var passwordCharacters = numericCharacters.concat(lowercaseCharacters, uppercaseCharacters);
+    } else if (includeSpecialCharacters.checked === true && includeNumericCharacters.checked === true && includeUppercaseCharacters.checked === true && includeLowercaseCharacters.checked === false){
+        var passwordCharacters = numericCharacters.concat(specialCharacters, uppercaseCharacters);
+    } else {
+        var passwordCharacters = numericCharacters.concat(specialCharacters, uppercaseCharacters, lowercaseCharacters);
+    };
+
+    for (var i = 1; i <= passwordLength.value; i++){
+        storedValue = storedValue + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    }
+
+    document.getElementById("passwordArea").innerHTML = storedValue;
+
+};
 
 function copyToClipboard() {
-    var copyText = document.getElementById("test");
+    var copyText = document.getElementById("passwordArea");
+    copyText.focus();
     copyText.select();
-    document.execCommand("copy")
-    alert("Copied")
-}
+    document.execCommand("copy");
+    alert("Copied");
+};
